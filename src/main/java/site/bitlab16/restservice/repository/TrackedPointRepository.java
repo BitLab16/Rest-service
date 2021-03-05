877c8d8c0e4dc56526cb8f291c782eb0012be4a8
Cezza99 <omodeicesare@gmail.com>
2021-03-05 22:05:37 +0100

    Refactoring model, repository and controller of TrackedPoint

--- src/main/java/site/bitlab16/restservice/repository/TrackedPointRepository.java
+++ src/main/java/site/bitlab16/restservice/repository/TrackedPointRepository.java
@@ -1,10 +1,27 @@
 package site.bitlab16.restservice.repository;
 
+import com.sun.istack.NotNull;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import site.bitlab16.restservice.model.TrackedPoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface TrackedPointRepository extends JpaRepository<TrackedPoint, Long>{
 
+    @Query(value = "WITH summary AS (\n" +
+            "    SELECT t.id,\n" +
+            "           t.code,\n" +
+            "           t.point_name,\n" +
+            "           t.description, \n" +
+            "           t.location \n" +
+            "    FROM tracked_point t)\n" +
+            "SELECT s.*\n" +
+            "FROM summary s\n" +
+            "WHERE s.id = :id", nativeQuery = true)
+    @Override
+    Optional<TrackedPoint> findById(@Param("id") Long id);
 }

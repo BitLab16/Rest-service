976e8522bc83ff7dee6e73532e267b3a726b0865
Samuele Vanini <samuele.vanini@live.it>
2021-03-07 21:35:07 +0100

    fix controller

--- src/main/java/site/bitlab16/restservice/controller/TrackedPointController.java
+++ src/main/java/site/bitlab16/restservice/controller/TrackedPointController.java
@@ -1,45 +1,49 @@
 package site.bitlab16.restservice.controller;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.bitlab16.restservice.exception.PointNotFoundException;
 import site.bitlab16.restservice.model.Gathering;
 import site.bitlab16.restservice.model.TrackedPoint;
-import site.bitlab16.restservice.repository.GatheringRepository;
-import site.bitlab16.restservice.repository.TrackedPointRepository;
+import site.bitlab16.restservice.service.GatheringService;
 import site.bitlab16.restservice.service.TrackedPointService;
 
 import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class TrackedPointController {
 
     private final TrackedPointService pointService;
-    private final GatheringRepository gatheringService;
+    private final GatheringService gatheringService;
 
-    public TrackedPointController(TrackedPointService pointService, GatheringRepository gatheringService) {
+    private final static Timestamp currTime = new Timestamp(1564255800000L);
+
+    public TrackedPointController(TrackedPointService pointService, GatheringService gatheringService) {
         this.pointService = pointService;
         this.gatheringService = gatheringService;
     }
 
     @GetMapping("/points")
-    List<Gathering> all() {
-        return gatheringService.findLastGatheringForAllPoint();
+    List<Gathering> allLastGatheringDetectedForAllPoint() {
+        return gatheringService.lastUpdate();
     }
 
-    @GetMapping("/points/{id}")
-    TrackedPoint one(@PathVariable Long id) {
+    @PostMapping("/points/{id}")
+    TrackedPoint pointDetailsBasedOnId(@PathVariable Long id) {
         return pointService.findById(id)
                 .orElseThrow(() -> new PointNotFoundException(id));
     }
 
-    @GetMapping("/pastData/{time}")
-    List<Gathering> pastData(Timestamp time){ return gatheringService.findPastGatherings(time); }
-
-    @GetMapping("/futureData/{time}")
-    List<Gathering> futureData(Timestamp time){ return gatheringService.findFutureGatherings(time); }
-
+    @PostMapping("/points/{time}")
+    List<Gathering> getGatheringBasedOnTimePassed(@PathVariable Timestamp time){
+        if(time.after(currTime))
+            return gatheringService.futureData(time);
+        else
+            return gatheringService.pastData(time);
+    }
 }
--- src/main/java/site/bitlab16/restservice/repository/GatheringRepository.java
+++ src/main/java/site/bitlab16/restservice/repository/GatheringRepository.java
@@ -12,16 +12,17 @@ import java.util.List;
 @Repository
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
 
-    @Query(value = "WITH summary AS (\n" +
-            "    SELECT g.id,\n" +
-            "           g.tracked_point_id,\n" +
-            "           g.detection_time,\n" +
-            "           ROW_NUMBER() OVER(PARTITION BY g.tracked_point_id\n" +
-            "               ORDER BY g.detection_time DESC) AS rk\n" +
-            "    FROM gatherings_detection g)\n" +
-            "SELECT s.*\n" +
-            "FROM summary s\n" +
-            "WHERE s.rk = 1", nativeQuery = true)
+    @Query(value = """
+                WITH summary AS (
+                    SELECT g.id,
+                       g.tracked_point_id,
+                       g.detection_time,
+                       ROW_NUMBER() OVER(PARTITION BY g.tracked_point_id
+                           ORDER BY g.detection_time DESC) AS rk
+                    FROM gatherings_detection g)
+                SELECT s.*
+                FROM summary s
+                WHERE s.rk = 1""", nativeQuery = true)
     List<Gathering> findLastGatheringForAllPoint();
 
     @Query(value = """
